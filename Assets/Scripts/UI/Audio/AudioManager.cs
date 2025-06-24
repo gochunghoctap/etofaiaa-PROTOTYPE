8cc5f3341baed8da5749b9076a0aa1d7de921ce6
trkiet <engtrinhduyam@gmail.com>
2025-06-24 15:40:10 +0900

    音量二重調整

--- Assets/Scripts/UI/Audio/AudioManager.cs
+++ Assets/Scripts/UI/Audio/AudioManager.cs
@@ -4,6 +4,13 @@ public class AudioManger : MonoBehaviour
 {
     void Awake()
     {
+        if (Object.FindObjectsByType<AudioManger>(FindObjectsSortMode.None).Length > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
         DontDestroyOnLoad(gameObject);
     }
+
 }
