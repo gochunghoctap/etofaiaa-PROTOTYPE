ef294765e9d95505a2f1f07e0ab8628bea858105
kobune <gochunghoctap@gmail.com>
2025-06-24 15:02:21 +0900

    scale fix

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
     public PlayerInput playerInput;
-    public float moveSpeed = 5f;
-    public float jumpForce = 7f;
+    private float moveSpeed = 5f;
+    private float jumpForce = 15f;
 
     private Rigidbody2D rb;
     private bool isGrounded = false;
