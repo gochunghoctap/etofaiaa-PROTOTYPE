a0cad3cd07e3df530acb23565a3eb93ac16595e3
hori akari <167387773+gochunghoctap@users.noreply.github.com>
2025-06-30 17:36:05 +0900

    微調整

--- Assets/Scripts/UI/Audio/AudioManager.cs
+++ Assets/Scripts/UI/Audio/AudioManager.cs
@@ -1,36 +1,16 @@
 using UnityEngine;
-using UnityEngine.Audio;
-using UnityEngine.UI;
 
-public class Audio : MonoBehaviour
+public class AudioManger : MonoBehaviour
 {
-    //Audioミキサーを入れるとこです
-    [SerializeField] AudioMixer audioMixer;
-
-    //それぞれのスライダーを入れるとこです。。
-    [SerializeField] Slider BGMSlider;
-    [SerializeField] Slider SESlider;
-
-    private void Start()
+    void Awake()
     {
-        //ミキサーのvolumeにスライダーのvolumeを入れてます。
+        if (Object.FindObjectsByType<AudioManger>(FindObjectsSortMode.None).Length > 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
 
-        //BGM
-        audioMixer.GetFloat("BGM", out float bgmVolume);
-        BGMSlider.value = bgmVolume;
-        //SE
-        audioMixer.GetFloat("SE", out float seVolume);
-        SESlider.value = seVolume;
-
-    }
-    public void SetBGM(float volume)
-    {
-        audioMixer.SetFloat("BGM", volume);
-    }
-
-    public void SetSE(float volume)
-    {
-        audioMixer.SetFloat("SE", volume);
+        DontDestroyOnLoad(gameObject);
     }
 
 }
