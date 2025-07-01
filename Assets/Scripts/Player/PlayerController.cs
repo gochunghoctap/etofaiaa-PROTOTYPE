7d4e62bfaa20a520752e7004dc77985340703c93
gochunghoctap <167387773+gochunghoctap@users.noreply.github.com>
2025-07-01 13:26:50 +0900

    walk speed

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
     public PlayerInput playerInput;
-    private float moveSpeed = 5f;
+    private float moveSpeed = 15f;
     private float jumpForce = 15f;
 
     private Rigidbody2D rb;
